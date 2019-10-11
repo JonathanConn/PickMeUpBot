68a145f6a559f5934b2ea3ca80bcebfe06e24357
Jonathan <Jonathanconn29@gmail.com>
2019-10-11 19:06:38 -0400

    working version needs timer setup

--- index.js
+++ index.js
@@ -5,54 +5,53 @@ const Twit = require('twit')
 const randomPuppy = require('random-puppy');
 const fs = require('fs'), request = require('request');
 
-var T = new Twit({
-  consumer_key: config.consumer_key,
-  consumer_secret: config.consumer_secret,
-  access_token_key: config.access_token,
-  access_token_secret: config.access_token_secret,
-}); 
+var T = new Twit(config); 
 
 var imgURL;
-
-var download = function(uri, filename, callback){
-  request.head(uri, function(err, res, body){
-    console.log('content-type:', res.headers['content-type']);
-    console.log('content-length:', res.headers['content-length']);
-    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
-  });
-};
 
 randomPuppy()
     .then(url => {
       imgURL = url;
-      console.log(url);
-})
+      console.log(imgURL);
 
-download(imgURL, 'puppy.png', function(){
-  console.log('finished downloading');
-});
+      var download = function(uri, filename, callback){
+        request.head(uri, function(err, res, body){
+          request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+        });
+      };
+      
+      download(imgURL, 'puppy.jpg', function(){
+        console.log('done');
 
-//
-// post a tweet with media
-//
-var b64content = fs.readFileSync('puppy.png', { encoding: 'base64' })
- 
-// first we must post the media to Twitter
-T.post('media/upload', { media_data: b64content }, function (err, data, response) {
-  // now we can assign alt text to the media, for use by screen readers and
-  // other text-based presentations and interpreters
-  var mediaIdStr = data.media_id_string
-  var altText = "puppy"
-  var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
- 
-  T.post('media/metadata/create', meta_params, function (err, data, response) {
-    if (!err) {
-      // now we can reference the media and post a tweet (media will attach to the tweet)
-      var params = { status: ':)', media_ids: [mediaIdStr] }
- 
-      T.post('statuses/update', params, function (err, data, response) {
-        console.log(data)
+              // post a tweet with media
+    //
+    var b64content = fs.readFileSync('./puppy.jpg', { encoding: 'base64' })
+    
+    // first we must post the media to Twitter
+    T.post('media/upload', { media_data: b64content }, function (err, data, response) {
+      // now we can assign alt text to the media, for use by screen readers and
+      // other text-based presentations and interpreters
+      var mediaIdStr = data.media_id_string
+      var altText = "puppy"
+      var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
+    
+      T.post('media/metadata/create', meta_params, function (err, data, response) {
+        if (!err) {
+          // now we can reference the media and post a tweet (media will attach to the tweet)
+          var params = { status: ':)', media_ids: [mediaIdStr] }
+    
+          T.post('statuses/update', params, function (err, data, response) {
+            console.log(data)
+          })
+        }
       })
-    }
-  })
+    })
+
+
+    
+
+      });
+      
 })
+
+  
